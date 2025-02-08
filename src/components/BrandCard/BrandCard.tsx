8d9a040bd45a61d752916a5b40d7cb6812c6b06a
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-08 23:02:24 +0200

    add brands data fetching initial functionality

--- src/components/BrandCard/BrandCard.tsx
+++ src/components/BrandCard/BrandCard.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { Brand } from "../../types";
 
-export default function BrandCard() {
-  return <div>BrandCard</div>;
+export default function BrandCard({ name, image }: Brand) {
+  return (
+    <div className="bg-white shadow-lg rounded-xl overflow-hidden p-4 transition-transform transform hover:scale-105">
+      {image ? (
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-32 object-cover rounded-lg"
+        />
+      ) : (
+        <div className="w-full h-32 flex items-center justify-center bg-gray-200 text-gray-500 text-sm rounded-lg">
+          No Image Available
+        </div>
+      )}
+      <h3 className="mt-2 text-lg font-semibold text-gray-800 text-center">
+        {name}
+      </h3>
+    </div>
+  );
 }
--- src/components/Brands/Brands.tsx
+++ src/components/Brands/Brands.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Brands as BrandsType } from "../../types";
+import fetchData from "../../utils/fetchData";
+import BrandCard from "../BrandCard/BrandCard";
 
 export default function Brands() {
-  return <div>Brands</div>;
+  const {
+    isLoading,
+    isFetching,
+    data: brandsData,
+    error,
+    isError,
+  } = useQuery<BrandsType>({
+    queryKey: ["brands"],
+    queryFn: () => fetchData<BrandsType>({ url: "/brands" }),
+    staleTime: 5 * 60 * 1000,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading || isFetching) {
+    return <div className="text-center text-gray-600">Loading...</div>;
+  }
+
+  if (isError) {
+    console.error(error);
+    return (
+      <div className="text-center text-red-500">Error fetching brands</div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {brandsData?.data?.map((brand) => (
+        <BrandCard key={brand._id} {...brand} />
+      ))}
+    </div>
+  );
 }
