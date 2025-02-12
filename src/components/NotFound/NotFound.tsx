de3127402e1f4900b31e80b57d3f82fa7f60c28f
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-13 00:08:24 +0200

    update not found component layout and styles

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <section className="mt-5 md:mt-10">
+      <div className="mx-auto max-w-screen-sm text-center">
+        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl font-kumbh text-primary-default drop-shadow">
+          404
+        </h1>
+        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
+          Something's missing.
+        </p>
+        <p className="">
+          Sorry, we can't find that page. You'll find lots to explore on the
+          home page.{" "}
+        </p>
+
+        <Link to="/" className="btn px-5 py-2.5 my-5 w-fit mx-auto">
+          Back to Homepage
+        </Link>
+      </div>
+    </section>
+  );
 }
