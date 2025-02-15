7ba485c812994089358d6f96037cfa03004d09bd
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-15 23:46:12 +0200

    add hero section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import Categories from "../Categories/Categories";
 import Products from "../Products/Products";
 import Hero from "./Hero";
+import Sale from "./Sale";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+
+      <Sale />
 
       <Categories />
 
