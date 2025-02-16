82a4f65ce2c48603ece629a8fda156c39607bc8e
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-16 19:56:41 +0200

    update change password validation schema error messages

--- src/schema/changePassword.ts
+++ src/schema/changePassword.ts
@@ -1,14 +1,14 @@
 import * as Yup from "yup";
 
 export const changePasswordSchema = Yup.object({
-  currentPassword: Yup.string().required("Password is required"),
+  currentPassword: Yup.string().required("current password is required"),
   password: Yup.string()
-    .required("Password is required")
+    .required("New password is required")
     .matches(
       /^[A-Za-z][A-Za-z0-9]{5,8}$/,
       "Password must be 6-9 characters, start with a letter, and contain only letters and numbers"
     ),
   rePassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords do not match")
-    .required("Confirm your password"),
+    .required("Confirm your new password"),
 });
