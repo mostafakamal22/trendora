8ba02916259e2ef003070d58ce6167a78570ebc3
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-16 20:02:42 +0200

    update edit user info validation schema error messages

--- src/schema/editUserInfo.ts
+++ src/schema/editUserInfo.ts
@@ -2,18 +2,18 @@ import * as Yup from "yup";
 
 export const editUserInfoSchema = Yup.object({
   name: Yup.string()
-    .required("Name is required")
+    .required("new name is required")
     .matches(
       /^[A-Za-z\s]{3,30}$/,
       "Name must be 3-30 characters and contain only letters and spaces"
     ),
   email: Yup.string()
-    .required("Email is required")
+    .required("new email is required")
     .email("Enter a valid email address"),
   phone: Yup.string()
     .matches(
       /^(?:\+20|0)?1[0125]\d{8}$/,
       "Enter a valid Egyptian phone number (e.g., 01012345678 or +201012345678)"
     )
-    .required("Phone number is required"),
+    .required("new phone number is required"),
 });
