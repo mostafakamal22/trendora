f1a3ccd692a0b77abf346e74036fe95b2633fc25
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-16 20:15:48 +0200

    handle setting route error messages in axios respose

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -4,6 +4,17 @@ export default function handleError(error: unknown): string {
   if (axios.isAxiosError(error)) {
     // Handle Axios-specific error
     console.error("Axios error:", error.response?.data || error.message);
+    // Handle setting route error message location in the response
+    if (
+      location?.pathname === "/setting" ||
+      location?.pathname === "/setting/"
+    ) {
+      return (
+        error.response?.data?.errors?.msg ||
+        "Invalid request. Please check your inputs and try again."
+      );
+    }
+    // Default error message for all routes
     return error.response?.data?.message || "Request failed";
   } else if (error instanceof Error) {
     // Handle generic JavaScript errors
